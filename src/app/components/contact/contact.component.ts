7764c6b438c5204de280994706c29d6346dcc12f
Luisgui <luisguirohi@gmail.com>
2019-03-04 21:49:36 -0500

    Headers y post request para crear contacto - falta autenticacion

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,10 +3,18 @@ import { Title }     from '@angular/platform-browser';//servicio para etqueta ti
 
 import { NgForm } from '@angular/forms';//modulo para control de formularios
 
-import { Http, Response } from '@angular/http';//importar componenete para hacer peticiones http
 import { Router, ActivatedRoute, Params } from '@angular/router';//leer informacion de ruas dinamicas
 import { ContactService } from '../../services/contact.service';//importar el servicio
 import { environment } from '../../../environments/environment';//importar las variables de ambiente
+
+import { Http, Response, Headers, RequestOptions } from '@angular/http';//importar componenete para hacer peticiones http
+
+let httpOptions = {
+  headers: new Headers({
+    'Content-Type':  'application/json',
+    'Authorization': 'g0Y2uVrK1ZzmOV9wBDHmnemaZBgTzFVXxkwYkrID'
+  })
+}
 
 @Component({
   selector: 'ed-contact',
@@ -15,9 +23,12 @@ import { environment } from '../../../environments/environment';//importar las v
 })
 export class ContactComponent implements OnInit {
 
+  env = environment.apiUrl;//importar rutas de apis relativas
+  public jsonUrl = this.env + "/types/contactos";
+
   constructor(
   	private titleService: Title,
-  	private http:Http,
+  	private http:Http
   	//private _contactService:ContactService 
   	) { }
 
@@ -26,7 +37,6 @@ export class ContactComponent implements OnInit {
   }
 
   contactForm(formulario:NgForm){
-  	console.log(formulario.value);
 
   	let msmBody = {
   		'title': formulario.value.email,
@@ -36,8 +46,8 @@ export class ContactComponent implements OnInit {
   		            <strong>Mensaje:</strong>,<br/> ${formulario.value.mensaje}`
   	};
 
-  	this.http.post('http://eldiablo.com.co/wp-json/wp/v2/posts', msmBody)
-  	         .subscribe( resp => ( console.log(resp) ) );
+  	this.http.post(this.jsonUrl, msmBody, httpOptions)
+  	.subscribe( resp => ( console.log(resp) ) );
 
   	//this._contactService.createContact(formulario.value)
   	                    //.subscribe( hero => this.heroes.push(formulario.value) );
